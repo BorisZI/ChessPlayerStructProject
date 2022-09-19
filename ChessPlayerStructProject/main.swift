ab094a53d96e7806224ab6dc509fea4b9ec6c09e
Boris Z <Boris Z>
2022-09-19 15:30:34 +0300

    FIN

--- ChessPlayerStructProject/main.swift
+++ ChessPlayerStructProject/main.swift
@@ -7,5 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+struct ChessPlayer {
+    var name: String
+    var  wins: Int
+    
+//    var description: String = ""
+    var description: String { "\(name) : \(wins)" }
+    
+    init(name: String, wins: Int) {
+        self.name = name
+        self.wins = wins
+    }
+    
+//    private mutating func getDescription() {
+//        description = "\(name) : \(wins)"
+//    }
+    
+    
+    mutating func addWins(_ a: Int) {
+        wins += a
+    }
+}
+
+var chessPlayerOne = ChessPlayer(name: "Boris", wins: 5)
+print(chessPlayerOne.description)
+chessPlayerOne.addWins(5)
+print(chessPlayerOne.description)
 
